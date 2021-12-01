4edefe5da5164898064e7cdf3b82704a35ace44b
Axel358 <edgar.serrano.9803@gmail.com>
2021-12-01 17:44:05 -0500

    Added more settings Implemented loading the docs from the website

--- Utils.py
+++ Utils.py
@@ -1,5 +1,17 @@
 import subprocess
 
+PROP_FREE_FORM = 'persist.waydroid.multiwindows'
+PROP_INVERT_COLORS = ''
+PROP_SUSPEND_INACTIVE = 'persist.waydroid.suspend'
+PROP_WINDOW_WIDTH = 'persist.waydroid.width'
+PROP_WINDOW_HEIGHT = 'persist.waydroid.height'
+PROP_WINDOW_WIDTH_PADDING = 'persist.waydroid.width_padding'
+PROP_WINDOW_HEIGHT_PADDING = 'persist.waydroid.height_padding'
+PROP_BLACKLISTED_APPS = 'waydroid.blacklist_apps'
+PROP_ACTIVE_APPS = 'waydroid.active_apps'
+DOCS_URL = 'https://docs.waydro.id/'
+HOME_URL = 'https://waydro.id/'
+
 
 def get_prop(name):
     try:
--- WaydroidSettings.py
+++ WaydroidSettings.py
@@ -3,7 +3,9 @@ import sys
 import gi
 import Utils
 gi.require_version('Gtk', '3.0')
+gi.require_version('WebKit2', '4.0')
 from gi.repository import Gtk
+from gi.repository import WebKit2
 
 
 class WaydroidSettings(Gtk.Application):
@@ -12,10 +14,31 @@ class WaydroidSettings(Gtk.Application):
 
         self.builder = Gtk.Builder.new_from_file('MainWindow.glade')
         self.window = None
-        self.free_form_switch = self.builder.get_object('free_form_switch')
 
-        if Utils.get_prop('persist.waydroid.multiwindows') == 'true':
-            self.free_form_switch.set_state(True)
+        docs_web_view_box = self.builder.get_object('docs_web_view_box')
+        self.docs_web_view = WebKit2.WebView()
+        docs_web_view_box.pack_start(self.docs_web_view, True, True, 0)
+
+        home_web_view_box = self.builder.get_object('home_web_view_box')
+        self.home_web_view = WebKit2.WebView()
+        home_web_view_box.pack_start(self.home_web_view, True, True, 0)
+
+        free_form_switch = self.builder.get_object('free_form_switch')
+        if Utils.get_prop(Utils.PROP_FREE_FORM) == 'true':
+            free_form_switch.set_state(True)
+
+        color_invert_switch = self.builder.get_object('color_invert_switch')
+        if Utils.get_prop(Utils.PROP_INVERT_COLORS) == 'true':
+            color_invert_switch.set_state(True)
+
+        suspend_switch = self.builder.get_object('suspend_switch')
+        if Utils.get_prop(Utils.PROP_SUSPEND_INACTIVE) == 'true':
+            suspend_switch.set_state(True)
+
+        w_width_entry = self.builder.get_object('w_width_entry')
+        w_height_entry = self.builder.get_object('w_height_entry')
+        wp_width_entry = self.builder.get_object('wp_width_entry')
+        wp_height_entry = self.builder.get_object('wp_height_entry')
 
         self.builder.connect_signals(self)
 
@@ -31,9 +54,48 @@ class WaydroidSettings(Gtk.Application):
 
     def toggle_free_form(self, switch, checked):
         if checked:
-            Utils.set_prop('persist.waydroid.multiwindows', 'true')
+            Utils.set_prop(Utils.PROP_FREE_FORM, 'true')
         else:
-            Utils.set_prop('persist.waydroid.multiwindows', 'false')
+            Utils.set_prop(Utils.PROP_FREE_FORM, 'false')
+
+    def toggle_suspend(self, switch, checked):
+        if checked:
+            Utils.set_prop(Utils.PROP_SUSPEND_INACTIVE, 'true')
+        else:
+            Utils.set_prop(Utils.PROP_SUSPEND_INACTIVE, 'false')
+
+    def toggle_color_inversion(self, switch, checked):
+        if checked:
+            Utils.set_prop(Utils.PROP_INVERT_COLORS, 'true')
+        else:
+            Utils.set_prop(Utils.PROP_INVERT_COLORS, 'false')
+
+    def show_ff_dialog(self, button):
+        dialog = Gtk.Dialog(title='Exclude apps')
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
+        )
+        dialog.get_content_area().pack_start(Gtk.Label(label='Space separated package names to exclude'), True, True,
+                                             10)
+        apps_entry = Gtk.Entry()
+        dialog.get_content_area().pack_start(apps_entry, True, True, 10)
+        dialog.show_all()
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            if len(app_list := apps_entry.get_text()) > 0:
+                Utils.set_prop(Utils.PROP_BLACKLISTED_APPS, app_list)
+
+        dialog.destroy()
+
+    def show_force_w_dialog(self, button):
+        pass
+
+    def on_tab_switched(self, notebook, page, position):
+        if position == 1:
+            self.docs_web_view.load_uri(Utils.DOCS_URL)
+        elif position == 2:
+            self.home_web_view.load_uri(Utils.HOME_URL)
 
 
 if __name__ == "__main__":
