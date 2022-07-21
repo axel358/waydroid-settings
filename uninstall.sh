09512fe40deb142dbacbbe2e8fbcdbcf543ba61f
Axel358 <edgar.serrano.9803@gmail.com>
2022-07-21 09:17:16 -0400

    Add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo rm -rf $HOME/.cache/waydroid-settings $HOME/.local/share/applications/waydroid-settings.desktop $HOME/.local/share/waydroid-settings /usr/share/waydroid-settings /usr/bin/waydroid-settings.sh /usr/local/share/applications/waydroid-settings.desktop /usr/local/share/applications/install-to-waydroid.desktop
+
+echo "Waydroid Settings has been uninstalled"
