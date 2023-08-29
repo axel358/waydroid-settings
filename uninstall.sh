8d0caa08374f36d085895d2d74db19adae36269d
Axel358 <edgar.serrano.9803@gmail.com>
2023-08-29 09:32:28 -0400

    refractor: UI and project structure

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-sudo rm -rf $HOME/.cache/waydroid-settings $HOME/.local/share/applications/waydroid-settings.desktop $HOME/.local/share/waydroid-settings /usr/share/waydroid-settings /usr/bin/waydroid-settings.sh /usr/local/share/applications/waydroid-settings.desktop /usr/local/share/applications/install-to-waydroid.desktop
+sudo rm -rf $HOME/.local/share/waydroid-settings /usr/share/waydroid-settings /usr/lib/waydroid-settings
+sudo rm -f /usr/bin/waydroid-settings
+sudo rm -f /usr/bin/waydroid-helper
+sudo rm -f /usr/share/applications/waydroid-settings.desktop
+sudo rm -f /usr/share/applications/install-to-waydroid.desktop
+sudo rm -f /usr/share/icons/hicolor/512x512/apps/cu.axel.WaydroidSettings.png
+sudo rm -f /usr/share/polkit-1/actions/org.freedesktop.policykit.waydroid-helper.policy
 
 echo "Waydroid Settings has been uninstalled"
--- usr/lib/waydroid-settings/waydroid-settings.py
+++ usr/lib/waydroid-settings/waydroid-settings.py
@@ -13,13 +13,16 @@ from gi.repository import Gtk, Vte, WebKit2, GLib, Gdk
 
 
 class WaydroidSettings(Gtk.Application):
+    DATA_DIR = '/usr/share/waydroid-settings/'
+    APP_ID = 'cu.axel.WaydroidSettings'
+
     def __init__(self, *args, **kargs):
-        super().__init__(*args, application_id='cu.axel.waydroidsettings', **kargs)
+        super().__init__(*args, application_id=self.APP_ID, **kargs)
 
         icon_theme = Gtk.IconTheme.get_default()
-        icon_theme.append_search_path('icons')
+        icon_theme.append_search_path(self.DATA_DIR + 'icons')
 
-        self.builder = Gtk.Builder.new_from_file('window.ui')
+        self.builder = Gtk.Builder.new_from_file(self.DATA_DIR + 'window.ui')
         self.window = None
         self.refreshing = False
 
@@ -37,11 +40,6 @@ class WaydroidSettings(Gtk.Application):
         self.free_form_switch.connect('state-set', self.toggle_free_form)
         self.nav_btns_switch.connect('state-set', self.toggle_navbar)
         self.soft_kb_switch.connect('state-set', self.toggle_keyboard)
-
-        w_width_entry = self.builder.get_object('w_width_entry')
-        w_height_entry = self.builder.get_object('w_height_entry')
-        wp_width_entry = self.builder.get_object('wp_width_entry')
-        wp_height_entry = self.builder.get_object('wp_height_entry')
 
         scripts_box = self.builder.get_object('scripts_box')
         scroll_view = Gtk.ScrolledWindow()
@@ -69,8 +67,6 @@ class WaydroidSettings(Gtk.Application):
         terminal_box.pack_end(scroll_view, True, True, 0)
 
         self.builder.connect_signals(self)
-        # Disable our password dialog for pkexec
-        # self.show_password_dialog()
         self.load_values()
 
     def load_values(self):
@@ -82,71 +78,17 @@ class WaydroidSettings(Gtk.Application):
         self.nav_btns_switch.set_active(utils.search_base_prop('qemu.hw.mainkeys=1'))
         self.soft_kb_switch.set_active(utils.is_kb_disabled())
 
-        settings_box = self.builder.get_object('settings_box')
+        container_banner = self.builder.get_object('container_banner')
+        session_banner = self.builder.get_object('session_banner')
+        container_banner.set_revealed(False)
+        session_banner.set_revealed(False)
+
         if not utils.is_container_active():
-            self.show_container_dialog()
-
-            hbox = Gtk.Box(spacing=6)
-            settings_box.add(hbox)
-            hbox.set_margin_top(2.5)
-            hbox.set_margin_bottom(2.5)
-
-            button = Gtk.Button.new_with_label("Start Container Service")
-            button.id = "get_waydroid_container_service_button"
-            button.connect("clicked", self.on_click_start_container)
-            hbox.pack_start(button, True, True, 10)
-
-        if not utils.is_waydroid_running():
-            self.show_not_available_dialog()
-
-            hbox = Gtk.Box(spacing=6)
-            settings_box.add(hbox)
-            hbox.set_margin_top(2.5)
-            hbox.set_margin_bottom(2.5)
-
-            button = Gtk.Button.new_with_label("Start Session")
-            button.connect("clicked", self.on_click_start_session)
-            hbox.pack_start(button, True, True, 10)
+            container_banner.set_revealed(True)
+        elif not utils.is_waydroid_running():
+            session_banner.set_revealed(True)
         else:
-            hbox = Gtk.Box(spacing=6)
-            settings_box.add(hbox)
-            hbox.set_margin_top(2.5)
-            hbox.set_margin_bottom(2.5)
-
-            button = Gtk.Button.new_with_label("Restart Container and Session")
-            button.id = "restart_waydroid_container_and_session_button"
-            button.connect("clicked", self.on_click_restart_session)
-            hbox.pack_start(button, True, True, 10)
-
-            hbox2 = Gtk.Box(spacing=6)
-            settings_box.add(hbox2)
-            hbox2.set_margin_top(2.5)
-            hbox2.set_margin_bottom(2.5)
-
-            button2 = Gtk.Button.new_with_label("Stop Session")
-            button2.id = "stop_waydroid_session_button"
-            button2.connect("clicked", self.on_click_stop_session)
-            hbox2.pack_start(button2, True, True, 10)
-
-            hbox3 = Gtk.Box(spacing=6)
-            settings_box.add(hbox3)
-            hbox3.set_margin_top(2.5)
-            hbox3.set_margin_bottom(2.5)
-
-            button3 = Gtk.Button.new_with_label("Freeze Container")
-            button3.id = "freeze_waydroid_container_button"
-            button3.connect("clicked", self.on_click_freeze_container)
-            hbox3.pack_start(button3, True, True, 10)
-
-            hbox4 = Gtk.Box(spacing=6)
-            settings_box.add(hbox4)
-            hbox4.set_margin_top(2.5)
-            hbox4.set_margin_bottom(2.5)
-
-            button4 = Gtk.Button.new_with_label("Unfreeze Container")
-            button4.id = "unfreeze_waydroid_container_button"
-            button4.connect("clicked", self.on_click_unfreeze_container)
-            hbox4.pack_start(button4, True, True, 10)
+            pass
 
         self.refreshing = False
 
@@ -180,12 +122,12 @@ class WaydroidSettings(Gtk.Application):
         utils.stop_session()
         self.load_values()
 
-    def on_click_freeze_container(self, button):
-        utils.freeze_container()
-        self.load_values()
+    def on_toggle_freeze_container(self, switch, checked):
+        if checked:
+            utils.freeze_container()
+        else:
+            utils.unfreeze_container()
 
-    def on_click_unfreeze_container(self, button):
-        utils.unfreeze_container()
         self.load_values()
 
     def do_activate(self):
@@ -271,69 +213,8 @@ class WaydroidSettings(Gtk.Application):
         dialog.destroy()
 
 
-    def show_password_dialog(self):
-        dialog = Gtk.Dialog(title='Root Permission Required', use_header_bar=True)
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
-        )
-        dialog.get_content_area().pack_start(Gtk.Label(label='Please enter root password'), True, True,
-                                             10)
-        pass_entry = Gtk.Entry()
-        pass_entry.set_visibility(False)
-        pass_entry.set_margin_start(5)
-        pass_entry.set_margin_end(5)
-
-        pass_entry.connect('activate', self.check_pass, pass_entry, dialog)
-
-        dialog.get_content_area().pack_start(pass_entry, True, True, 10)
-        dialog.show_all()
-        response = dialog.run()
-
-        if response == Gtk.ResponseType.OK:
-            self.check_pass(None, pass_entry, dialog)
-        else:
-            dialog.destroy()
-
-
-    def check_pass(self, widget, pass_entry, dialog):
-        if len(password := pass_entry.get_text().strip()) > 0:
-            if utils.is_correct_pass(password):
-                dialog.destroy()
-            else:
-                self.show_wrong_password_dialog()
-                dialog.destroy()
-                self.show_password_dialog()
-
-    def show_wrong_password_dialog(self):
-        dialog = Gtk.MessageDialog(transient_for=self.window)
-        dialog.props.message_type=Gtk.MessageType.INFO
-        dialog.props.title = 'Authorization Issue'
-        dialog.props.text = 'The password you entered was not correct. Please try again'
-        dialog.add_buttons('OK', Gtk.ResponseType.OK)
-        dialog.run()
-        dialog.destroy()
-        # ~ self.show_password_dialog()
-
-    def show_not_available_dialog(self):
-        dialog = Gtk.MessageDialog(transient_for=self.window)
-        dialog.props.message_type=Gtk.MessageType.INFO
-        dialog.props.title = 'Waydroid session not running'
-        dialog.props.text = 'Waydroid is either not installed or currently not running. Please start the session using the option at the bottom of the next screen. '
-        dialog.add_buttons('OK', Gtk.ResponseType.OK)
-        dialog.run()
-        dialog.destroy()
-
-    def show_container_dialog(self):
-        dialog = Gtk.MessageDialog(transient_for=self.window)
-        dialog.props.message_type=Gtk.MessageType.INFO
-        dialog.props.title = 'Waydroid container not running'
-        dialog.props.text = 'Waydroid container service is currently not running or waydroid is not installed properly. Please start the service using the option at the bottom of the next screen. '
-        dialog.add_buttons('OK', Gtk.ResponseType.OK)
-        dialog.run()
-        dialog.destroy()
-
     def show_resize_dialog(self, button):
-        dialog_builder = Gtk.Builder().new_from_file('resize_dialog.ui')
+        dialog_builder = Gtk.Builder().new_from_file(self.DATA_DIR + 'resize_dialog.ui')
         dialog_box = dialog_builder.get_object('resize_dialog_box')
         dialog = Gtk.Dialog(title='Resize system image', use_header_bar=True)
         dialog.add_buttons(
@@ -419,10 +300,10 @@ class WaydroidSettings(Gtk.Application):
     def show_about_dialog(self, button):
         dialog = Gtk.AboutDialog()
         dialog.props.program_name = 'Waydroid Settings'
-        dialog.props.version = "0.1.0"
+        dialog.props.version = "0.3.0"
         dialog.props.authors = ['Axel358', 'Jon West']
         dialog.props.copyright = 'GPL-3'
-        dialog.props.logo_icon_name = 'waydroid-settings'
+        dialog.props.logo_icon_name = self.APP_ID
         dialog.props.comments = 'Control Waydroid settings'
         dialog.props.website = utils.HOME_URL
         dialog.set_transient_for(self.window)
