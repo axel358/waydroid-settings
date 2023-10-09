e25467d98cf94fd4d390c9bf7791d21941777798
firefoxlover <78762110+firefoxlover@users.noreply.github.com>
2023-10-09 21:57:20 +0000

    variable immutable-friendly directories
    Unsure about the polkit rules.
    
    The immutable version is work in progress, the current way is unchanged, again.
    
    Afaik one should not write polkit rules to /usr but to /etc in general

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
+# Check if the system is immutable and set directory variables.
+if sudo touch /bin/testfile 2>/dev/null; then
+    # System is not immutable
+    sudo rm -f /bin/testfile
+    bindir="/usr/bin"
+    sharedir="/usr/share"
+    applicationsdir="/usr/share/applications"
+    polkitdir="/usr/share/polkit-1"
+else
+    # System is immutable
+    bindir="$HOME/.local/bin"
+    sharedir="$HOME/.local/share"
+    applicationsdir="$HOME/.local/share/applications"
+    polkitdir="/etc/polkit-1/"
+fi
+
 sudo rm -rf $HOME/.local/share/waydroid-settings /usr/share/waydroid-settings /usr/lib/waydroid-settings
-sudo rm -f /usr/bin/waydroid-settings
-sudo rm -f /usr/bin/waydroid-helper
-sudo rm -f /usr/share/applications/waydroid-settings.desktop
-sudo rm -f /usr/share/applications/install-to-waydroid.desktop
-sudo rm -f /usr/share/icons/hicolor/512x512/apps/cu.axel.WaydroidSettings.png
-sudo rm -f /usr/share/polkit-1/actions/org.freedesktop.policykit.waydroid-helper.policy
+sudo rm -f $bindir/waydroid-settings
+sudo rm -f $bindir/waydroid-helper
+sudo rm -f $applicationsdir/waydroid-settings.desktop
+sudo rm -f $applicationsdir/install-to-waydroid.desktop
+sudo rm -f $sharedir/icons/hicolor/512x512/apps/cu.axel.WaydroidSettings.png
+sudo rm -f $polkitdir/actions/org.freedesktop.policykit.waydroid-helper.policy
 
 echo "Waydroid Settings has been uninstalled"
